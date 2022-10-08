40f05a4a255cf59f18b14b16f7259b6430d8a4f6
zeeshan <skzeeshan3650@gmail.com>
2022-10-08 21:36:16 +0530

    commit 7

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <malloc.h>
+
+/* typedef struct
+{
+    int arrSize;
+    int array[];
+} myArray; */
+
+/* int main()
+{
+    int size, size1;
+    myArray *ptr;
+
+    printf("Enter length of array: ");
+    scanf("%d", &size);
+
+    ptr = malloc(sizeof(myArray) + size * sizeof(int));
+
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", ptr+i);
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+       printf("Element %d: %d\n", i+1, *(ptr+i));
+    }
+    
+    
+    free(ptr);
+    return 0;
+} */
+
+int main()
+{
+    #include <complex.h>
+    #ifdef __STDC_NO_COMPLEX__
+    printf("Complex arithmetic not supported\n");
+    #else
+    printf("Complex arithmetic supported\n");
+
+    double complex cx = 1.0 + 3.0*I;
+    double complex cy = 1.0 - 4.0*I;
+    double complex sum;
+
+    sum = cx + cy;
+    printf("Sum: (%.1f%+.1f) + (%.1f%+.1f) = %.1f%+.1f\n", creal(cx), cimag(cx), creal(cy), cimag(cy), creal(sum), cimag(sum));
+
+    sum = cx - cy;
+    printf("Difference: (%.1f%+.1f) - (%.1f%+.1f) = %.1f%+.1f\n", creal(cx), cimag(cx), creal(cy), cimag(cy), creal(sum), cimag(sum));
+
+    sum = cx * cy;
+    printf("Product: (%.1f%+.1f) X (%.1f%+.1f) = %.1f%+.1f\n", creal(cx), cimag(cx), creal(cy), cimag(cy), creal(sum), cimag(sum));
+
+    sum = cx / cy;
+    printf("Division: (%.1f%+.1f) / (%.1f%+.1f) = %.1f%+.1f\n", creal(cx), cimag(cx), creal(cy), cimag(cy), creal(sum), cimag(sum));
+
+    double complex conjugate_CX = conj(cx);
+    double complex conjugate_CY = conj(cy);
+    printf("Conjugate CX: %.1f%+.1f\nConjugate CY: %.1f%+.1f\n", creal(conjugate_CX), cimag(conjugate_CX), creal(conjugate_CY), cimag(conjugate_CY));
+#endif
+}
